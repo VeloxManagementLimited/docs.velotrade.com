7635b8c5fd89d891fd73a6e10958bee47386c7ca
Robin Walser <robin.walser@me.com>
2017-10-09 22:55:43 +0800

    small update

--- pages/header.php
+++ pages/header.php
@@ -3,6 +3,7 @@
     <div class="container">
         <div class="row">
             <h3 class="title">Public API SDK</h3>
+            <br>
             <h5>Do you want to integrate Velotrade into your application? We provide a REST API and Java SDK that will help you complete your integration quickly and easy.</h5>
         </div>
     </div>
